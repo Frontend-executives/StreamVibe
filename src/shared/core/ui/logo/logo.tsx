4ca75cde7f689761ed8e43b6c68fd045b9cad5ac
Kirill <qtrixnet@yandex.ru>
2024-03-02 11:55:31 +0600

    feat/header: add tailwind comments

--- src/shared/core/ui/logo/logo.tsx
+++ src/shared/core/ui/logo/logo.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link'
 import { ReactElement } from 'react'
 
 const className = cx(
+  //* Типографика
   'text-4xl font-extrabold',
+  //* Оформление
   'bg-gradient-to-r from-purple-300 to-pink-600 inline-block text-transparent bg-clip-text',
+  //* Анимация
   'select-none hover:opacity-80 transition-opacity',
 )
 
